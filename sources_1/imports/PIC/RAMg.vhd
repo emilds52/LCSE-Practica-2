27d32e855f731a44ad5300f93cc2b2d0daccc7bb
emilds52 <emilds@live.com>
2023-10-10 18:05:38 +0200

    Ram skeleton

--- sources_1/imports/PIC/RAMg.vhd
+++ sources_1/imports/PIC/RAMg.vhd
@@ -5,22 +5,37 @@ USE IEEE.numeric_std.all;
 
 USE work.PIC_pkg.all;
 
-ENTITY ram IS
+ENTITY ramg IS
 PORT (
    Clk      : in    std_logic;
-   Reset    : in    std_logic;
    write_en : in    std_logic;
    oe       : in    std_logic;
    address  : in    std_logic_vector(7 downto 0);
    databus  : inout std_logic_vector(7 downto 0));
-END ram;
+END ramg;
 
-ARCHITECTURE behavior OF ram IS
+ARCHITECTURE behavior OF ramg IS
 
   SIGNAL contents_ram : array8_ram(255 downto 64);
 
 BEGIN
 
+CS_RAMg <= '1' when (adress(7) or adress(6)) else '0';
+
+-------------------------------------------------------------------------
+-- Memoria de prop�sito general
+-------------------------------------------------------------------------
+p_ram : process (clk)  -- no reset
+begin
+  if clk'event and clk = '1' then
+    if write_en = '1' and CS_RAMg = '1' then
+      contents_ram(to_integer(unsigned(address))) <= databus;
+    end if;
+  end if;
+end process;
+
+databus <= contents_ram(to_integer(unsigned(address))) when oe = '0' and CS_RAMg = '1' else (others => 'Z');
+-------------------------------------------------------------------------
 
 -------------------------------------------------------------------------
 -- Decodificador de BCD a 7 segmentos
