7a57c15520eca6256cb171758584d96586ef4466
Vml312 <valentinmtnz99@gmail.com>
2023-10-26 17:32:51 +0200

    Update DMA.vhd

--- sources_1/imports/PIC/DMA.vhd
+++ sources_1/imports/PIC/DMA.vhd
@@ -37,6 +37,7 @@ type state_t is (
   idle,
   request,
   writing,
+  write_FF,
   request_end,
   pretransmision_1,
   transmision_1,
@@ -49,7 +50,8 @@ signal current_state_reg : state_t;
 signal Data_Read_reg: std_logic;
 signal Valid_D_reg: std_logic;
 signal TX_Data_reg: std_logic_vector(7 downto 0);
-signal Address_reg: std_logic_vector(7 downto 0);
+signal Address_reg: unsigned(2 downto 0); --solo se utiliza hasta x"05"
+signal byte_count: unsigned(2 downto 0); --solo se utiliza hasta x"03"
                                                 
 signal Databus_reg: std_logic_vector(7 downto 0);
 signal Write_en_reg: std_logic;
@@ -65,92 +67,99 @@ begin
       Data_Read_reg <= '0';
       Valid_D_reg <= '1';--activa a nivel bajo
       TX_Data_reg <= (others => '0');
-      Address_reg <= x"00";
-      databus_reg <= (others=> '0');
+      Address_reg <= (others => '0');
+      databus_reg <= (others => '0');
       Write_en_reg <= '0';
       OE_reg <= '0';
       DMA_RQ_reg <= '0';
-      READY_reg <= '0'; --procesador no ocioso
+      READY_reg <= '1'; --procesador no ocioso
       current_state_reg <= idle;
             
     elsif rising_edge(clk) then
       case current_state_reg is
     
         when idle=>
-          READY_reg <= '1';
-          Address_reg <= x"00";
+          READY_reg <= '1'; -- se puede omitir
           if Send_Comm = '1' then 
             current_state_reg <= pretransmision_1;
             READY_reg <= '0';
-          elsif RX_Empty = '0' then--si memoria interna no vacï¿½a, recibe datos
+            OE_reg <= '1';
+            Address_reg <= to_unsigned(4, Address_reg'length);
+          elsif RX_Empty = '0' and Send_Comm = '0' then--si memoria interna no vacï¿½a, recibe datos
             --siempre vienen 3 bytes? El orden es por llegada(MSB, Int, LSB)
             current_state_reg <= request;
-            READY_reg <= '0';
+            DMA_RQ_reg <= '1';
           end if;
         
         when request=>
-          DMA_RQ_reg <= '1';--Peticiï¿½n de buses
           if DMA_ACK = '1' then
             current_state_reg <= writing;
             Write_en_reg <= '1';
             Data_Read_reg <= '1';
-            Address_reg <= x"00";
+            Address_reg <= byte_count;
+            Databus_reg <= RCVD_Data;
+            byte_count <= byte_count + 1;
           end if;
         
         when writing=>        
           --Data_Read_reg <= '1';--hay que hacerla un ciclo antes para haber recibido el byte
           --Write_en_reg <= '1';--hay que hacerlo un ciclo antes
-          if RX_Empty = '0' then
+          if RX_Empty = '0' and byte_count < 3 then --x"03"
             databus_reg <= RCVD_Data;
-            Address_reg <= Address_reg + 1;
-            if Address_reg = x"03" then
-              current_state_reg <= request_end;
-              Write_en_reg <= '0';
-              Data_Read_reg <= '0';
-              databus_reg <= x"FF";
-            end if;
-          else
-            Address_reg <= x"03";--si son menos de 3 bytes, solo se ocupa de 0x00 a donde llegue, se mantienen los que no llegue --Es decir, si es uno, se mantienen intermeido y LSB como estaban o se ponen a cero?
+            Address_reg <= byte_count;
+            byte_count <= byte_count + 1;
+          end if;
+          if byte_count = x"03" then
+            current_state_reg <= write_FF;
+            Address_reg <= byte_count;
+            byte_count <= (others=>'0');
+            Data_Read_reg <= '0';
             databus_reg <= x"FF";
+          end if;
+          if RX_Empty = '1' and byte_count < 3 then 
+            Address_reg <= (others=>'0');
             current_state_reg <= request_end;
             Write_en_reg <= '0';
             Data_Read_reg <= '0';
+            DMA_RQ_reg <= '0';
           end if;
+        
+        when write_FF=>
+          Address_reg <= (others=>'0');
+          DMA_RQ_reg <= '0';
+          Write_en_reg <= '0';
+          current_state_reg <= request_end;
           
         when request_end=>
-          DMA_RQ_reg <= '0';
           if DMA_ACK = '0' then
             current_state_reg <= idle;
-            READY_reg <= '1';
           end if;
             
         when pretransmision_1=>--se puede poner en *1
         --se supone TX_RDY en 1
           Valid_D_reg <= '0';
-          Address_reg <= x"04";
-          OE_reg <= '0';--habilitaciï¿½n nivel bajo
+          TX_Data_reg <= Databus;
           current_state_reg <= transmision_1;
           
         when transmision_1=>
-          TX_Data_reg <= Databus_reg;
           if ACK_out = '0' then--activa a nivel bajo, llegada a RS232
             current_state_reg <= pretransmision_2;
             Valid_D_reg <= '1';
-            OE_reg <= '1';
+            Address_reg <= to_unsigned(5, Address_reg'length);
           end if;
           
         when pretransmision_2=>--se puede poner antes
           Valid_D_reg <= '0';
-          Address_reg <= x"05";
-          OE_reg <= '0';--habilitaciï¿½n nivel bajo
+          TX_Data <= Databus;
+          --OE_reg <= '0'; -- se puede poner aquí
           current_state_reg <= transmision_2;
           
         when transmision_2=>
-          TX_Data_reg <= Databus_reg;
           if ACK_out = '0' then--activa a nivel bajo, llegada a RS232
             current_state_reg <= idle;
-            Valid_D <= '1';
-            OE_reg <= '1';
+            Address_reg <= (others=>'0');
+            Valid_D_reg <= '1';
+            OE_reg <= '0';
           end if;
       end case;
     end if;
@@ -161,7 +170,7 @@ begin
   Data_Read <= Data_Read_reg;
   Valid_D <= Valid_D_reg;
   TX_Data <= TX_Data_reg;
-  Address <= Address_reg;
+  Address <= "00000" & std_logic_vector(Address_reg);
 --  Databus <= Databus_reg;
   Databus <= Databus_reg when write_en_reg='1' else (others=> 'Z'); --Sale XXX en simulaciï¿½n
 
